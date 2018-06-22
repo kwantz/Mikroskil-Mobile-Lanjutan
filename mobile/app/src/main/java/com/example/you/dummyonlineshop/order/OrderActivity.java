85efc7ee0580cd11c81694b94d0c5f9813d95dc1
Kwantz <erickkwantantz123@gmail.com>
2018-06-22 20:58:41 +0700

    Add all activities

---
+++ mobile/app/src/main/java/com/example/you/dummyonlineshop/order/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.you.dummyonlineshop.order;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.you.dummyonlineshop.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+    }
+}
